30f2a115a1117821c459dcb7360bef0211acb264
johannes <mojohannes06@gmail.com>
2024-09-09 21:57:03 +0200

    FullREsponisive exp and projects

--- FrontEnd/src/pages/Home/Projects.jsx
+++ FrontEnd/src/pages/Home/Projects.jsx
@@ -6,12 +6,12 @@ const Projects = () => {
   const [selectedProject, setSelectedProject] = useState(0);
 
   return (
-    <div className="h-[80vh] pb-8 w-full flex flex-col gap-8">
+    <div className="h-[100vh] pb-16 w-full flex flex-col gap-8">
       <Section title="Projects" />
       
-      <div className="flex flex-col w-full md:flex-row h-full">
+      <div className="flex flex-col w-full md:gap-36 pt-12 md:flex-row h-full">
         {/* Scrollable container for project names */}
-        <div className="w-full md:overflow-x-auto flex items-center space-x-4 pb-2">
+        <div className="w-full md:w-[unset]   overflow-x-auto md:overflow-hidden flex md:flex-col items-cente md:space-x-0 space-x-4 pb-2">
           {ProjectsData.map((data, index) => (
             <div
               key={index}
@@ -20,13 +20,13 @@ const Projects = () => {
                 ${selectedProject === index ? 'bg-[#5CCFE6] bg-opacity-20 text-secondary-secondary-2 font-semibold' : 'hover:bg-opacity-10 hover:bg-gray-600'}
               `}
             >
-              <h1 className="text-center">{data.name}</h1>
+              <h1 className="">{data.name}</h1>
             </div>
           ))}
         </div>
 
         {/* Project details */}
-        <div className="rightExp flex-1 bg-gray-800 p-6 rounded-md shadow-lg">
+        <div className="rightExp flex-1 h-fit bg-gray-800 p-6 rounded-md shadow-lg">
           {ProjectsData.map((data, index) => (
             <div key={index} className={`${selectedProject === index ? 'block' : 'hidden'} flex flex-col gap-4`}>
               <h1 className="text-secondary-secondary-3 text-2xl font-semibold">{data.name}</h1>
