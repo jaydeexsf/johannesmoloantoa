2aa96ef29b69d6973be920a89925c1ff119b33a7
jaydeexsf <140901459+jaydeexsf@users.noreply.github.com>
2024-10-12 06:54:57 +0200

    Update experienceData.jsx

--- FrontEnd/src/assets/resources/experienceData.jsx
+++ FrontEnd/src/assets/resources/experienceData.jsx
@@ -1,4 +1,28 @@
 export const ExperienceDat = [
+   {
+    "title": "Full-Stack Developer",
+    "company": "OffCampusRooms",
+    "location": "Polokwane, South Africa",
+    "period": "Augaust 2024 - Present",
+    "description": "Led the development of OffCampusRooms, a platform helping students near the University of Limpopo find off-campus accommodation. Implemented a search and filtering system for users to easily find and connect with  landlords based on their preferences and budget.",
+    "responsibilities": [
+      "Designed and developed the full-stack web application using ReactJS and Firebase.",
+      "Integrated real-time listings, allowing users to browse available rooms with updated details.",
+      "Built user authentication and role-based access for students and landlords.",
+      "Optimized the application for mobile and desktop, ensuring accessibility."
+    ],
+    "tools": [
+      "ReactJS",
+      "Firebase",
+      "Tailwind CSS",
+      "JavaScript",
+      "GitHub"
+    ],
+    "achievements": [
+      "Successfully onboarded over 100 landlords, listing available rooms within the first month.",
+      "Increased platform traffic by 30% through responsive design and user-friendly interfaces."
+    ]
+  },
   {
     "title": "Freelance   Full-Stack Developer",
     "company": "Jimney's Fish & Chips",
@@ -23,30 +47,6 @@ export const ExperienceDat = [
       "Improved user experience, reducing bounce rates by 15%."
     ]
   },
-  {
-    "title": "Full-Stack Developer",
-    "company": "OffCampusRooms",
-    "location": "Polokwane, South Africa",
-    "period": "Augaust 2024 - Present",
-    "description": "Led the development of OffCampusRooms, a platform helping students near the University of Limpopo find off-campus accommodation. Implemented a search and filtering system for users to easily find and connect with  landlords based on their preferences and budget.",
-    "responsibilities": [
-      "Designed and developed the full-stack web application using ReactJS and Firebase.",
-      "Integrated real-time listings, allowing users to browse available rooms with updated details.",
-      "Built user authentication and role-based access for students and landlords.",
-      "Optimized the application for mobile and desktop, ensuring accessibility."
-    ],
-    "tools": [
-      "ReactJS",
-      "Firebase",
-      "Tailwind CSS",
-      "JavaScript",
-      "GitHub"
-    ],
-    "achievements": [
-      "Successfully onboarded over 100 landlords, listing available rooms within the first month.",
-      "Increased platform traffic by 30% through responsive design and user-friendly interfaces."
-    ]
-  }
 ];
 
 export default { ExperienceDat };
