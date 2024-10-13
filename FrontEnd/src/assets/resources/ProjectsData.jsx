0c8c3715edc12cc929985a9441307bcf981a2d45
jaydeexsf <140901459+jaydeexsf@users.noreply.github.com>
2024-10-13 03:33:13 +0200

    Update ProjectsData.jsx

--- FrontEnd/src/assets/resources/ProjectsData.jsx
+++ FrontEnd/src/assets/resources/ProjectsData.jsx
@@ -7,6 +7,13 @@ const ProjectsIn = [
 //   "technologies": ["React", "Tailwind CSS", "Firebase", "Node.js", "Express"]
 // },
   {
+    "name": "OffCampusRooms",
+    "description": "A web application that helps students at the University of Limpopo find the nearest classes and buildings using OpenStreetMap data and Dijkstra's Algorithm for navigation.",
+    "sourceCode": "https://github.com/jaydeexsf/OffCampusRooms",
+    "demoLink": "https://off-campus-rooms.vercel.app/",
+    "technologies": ["ReactJS", "NodeJS", "Tailwind CSS", "ExpressJS", "MongoDB"]
+  }, 
+  {
     "name": "Mankweng Market",
     "description": "A marketplace for sellers near the University of Limpopo to list furniture and other items for sale, allowing students and buyers to browse and contact sellers. Built using the MERN stack for full functionality.",
     "sourceCode": "https://github.com/jaydeexsf/LocalMarket",
@@ -19,13 +26,6 @@ const ProjectsIn = [
     "sourceCode": "https://github.com/jaydeexsf/All4Queens",
     "demoLink": "https://jaydeexsf.github.io/All4Queens/",
     "technologies": ["React", "Tailwind CSS", "Firebase"]
-  },
-  {
-    "name": "OffCampusRooms",
-    "description": "A web application that helps students at the University of Limpopo find the nearest classes and buildings using OpenStreetMap data and Dijkstra's Algorithm for navigation.",
-    "sourceCode": "https://github.com/jaydeexsf/OffCampusRooms",
-    "demoLink": "https://off-campus-rooms.vercel.app/",
-    "technologies": ["ReactJS", "NodeJS", "Tailwind CSS", "ExpressJS", "MongoDB"]
   },
 ];
 
